ce1763754d3cfcd2eeebe08e53bcdcc9b39268c6
Billy Lam <billyl99@gmail.com>
2013-04-03 08:25:38 -0700

    Another quick commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
     $.getScript("parser.js");
+    
+    var currentEquation;
 
     $('#addEquation').click(function(){
         var equation = $('input[name="equation"]').val();
         // validate equation with parse
         // http://silentmatt.com/javascript-expression-evaluator/
         var expression = Parser.parse(equation);
-        $('#equationList').append('<li>' + expression + '</li>');
+        $('#equationList').append('<li>' + expression + '</li><br />');
     });
 
+    $(document).on('click', 'li', function(){
+        $('li').removeClass('highlighted');
+        $(this).toggleClass('highlighted');
+        currentEquation = Parser.parse($(this).text());
+    });
+    
+    function calculate() {
+        console.log(currentEquation);
+    };
 });
