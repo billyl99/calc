93626f54d50018bed5454ca75f56f8ed5937333d
Billy Lam <billyl99@gmail.com>
2013-04-03 11:27:34 -0700

    Logic works, but needs a legitimate UI

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ $(document).ready(function() {
     $.getScript("parser.js");
     
     var currentEquation;
+	var variables;
 
     $('#addEquation').click(function(){
         var equation = $('input[name="equation"]').val();
-        // validate equation with parse
+        // validate equation with equation parse
         // http://silentmatt.com/javascript-expression-evaluator/
         var expression = Parser.parse(equation);
         $('#equationList').append('<li>' + expression + '</li><br />');
     });
 
     $(document).on('click', 'li', function(){
+		$('#variableList').empty();
+	
         $('li').removeClass('highlighted');
         $(this).toggleClass('highlighted');
         currentEquation = Parser.parse($(this).text());
+		variables = currentEquation.variables();
+		
+		for (var i = 0; i < variables.length; i++) {
+			$('#variableList').append('<input type="text" name=' + variables[i] + ' value="0""> <label>' + variables[i] + '</label> <br />');
+		}
     });
-    
-    function calculate() {
-        console.log(currentEquation);
-    };
+	
+	$(document).on('keyup', '#variableList input', function(){
+		var result;
+		var variablesTable = {};
+		$('#variableList').children('input').each(function(){
+			variablesTable[$(this).attr("name")] = $(this).val();
+		});
+		$('#answerbox').text(currentEquation.evaluate(variablesTable));
+	});
 });
